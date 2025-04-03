28305d9de3323fdf5f30697f2942a39727628677
craftgamerbog <craftgamerbog678@gmail.com>
2025-04-03 17:51:04 -0300

    Add files via upload

---
+++ #Aula02.py
@@ -0,0 +1,33 @@
+#Aula02
+#Exercicio 1
+#numero = int(input("Digite um numero:"))
+#if numero % 2 == 0:
+#    print("O numero é par")
+#else:print("O numero é impar")
+
+
+#Exercicio 2
+#numero = int(input("Digite um numero:"))
+#numero2 = int(input("Digite um numero:"))
+#if numero > numero2:
+#    print("O primeiro numero é maior")
+#else: numero < numero2, print("O segundo numero é maior")
+
+
+#Exercicio 3
+#Numero = int(input("Qual é sua idade:"))
+#Idade = int(18)
+#if Idade < Numero: 
+#	print("Você tem idade suficiente para dirigir")  
+#else: print("Você não tem idade suficiente para dirigir")
+
+
+#Exercicio 4
+#numero = int(input("digite um numero?"))
+#if numero > 0:
+#  print("Seu nomero é positivo")
+#elif numero < 0:
+# print("Seu numero é negativo")
+#else: print("Seu numero é zero")
+
+
